e728b65a64a6dfbc4ff7e8fa6e8bae251a0260ac
Tim Z <zhenghanecho@gmail.com>
2017-10-12 21:51:01 -0400

    Update

---
+++ lab6.py
@@ -0,0 +1,44 @@
+'''
+Created on _______________________
+@author:   _______________________
+Pledge:    _______________________
+
+CS115 - Lab 6
+'''
+def isOdd(n):
+    '''Returns whether or not the integer argument is odd.'''
+    pass  # TODO
+
+def numToBinary(n):
+    '''Precondition: integer argument is non-negative.
+    Returns the string with the binary representation of non-negative integer n.
+    If n is 0, the empty string is returned.'''
+    pass  # TODO
+
+def binaryToNum(s):
+    '''Precondition: s is a string of 0s and 1s.
+    Returns the integer corresponding to the binary representation in s.
+    Note: the empty string represents 0.'''
+    pass  # TODO
+
+def increment(s):
+    '''Precondition: s is a string of 8 bits.
+    Returns the binary representation of binaryToNum(s) + 1.'''
+    pass  # TODO
+
+def count(s, n):
+    '''Precondition: s is an 8-bit string and n >= 0.
+    Prints s and its n successors.'''
+    pass  # TODO
+
+def numToTernary(n):
+    '''Precondition: integer argument is non-negative.
+    Returns the string with the ternary representation of non-negative integer
+    n. If n is 0, the empty string is returned.'''
+    pass  # TODO
+
+def ternaryToNum(s):
+    '''Precondition: s is a string of 0s, 1s, and 2s.
+    Returns the integer corresponding to the ternary representation in s.
+    Note: the empty string represents 0.'''
+    pass  # TODO
