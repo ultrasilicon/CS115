043cb9aa0eee578321da879261d5a5f061105cf9
Tim Z <zhenghanecho@gmail.com>
2017-10-18 10:25:32 -0400

    Update

--- hw6.py
+++ hw6.py
@@ -24,6 +24,7 @@ def numToBinary(n):
     return numToBinary(int(n / 2)) + str(n%2)
 
 def completeFiveDigit(s):
+    '''complete 5 digit if the binary string is not long enough, fill higher digits with 0.'''
     return "0" * (5 - len(s)) + s
 
 def binaryToNum(s):
@@ -35,42 +36,47 @@ def binaryToNum(s):
     return binaryToNum(s[:-1]) * 2 + int(s[-1])
 
 def compress(s):
+    '''compress binary string and return compressed binary string'''
     def compress_helper(s, buf):
+        '''return an array, whose members are arrays in format of ["0", 0], the first element indicates whether it is 1 or 0, the second element indicates the number of 1 or 0 existing consecutively.'''
         if s == "":
             return [buf]
-        if s[0] != buf[0]:
-            if buf[1] != 0:
+        if s[0] != buf[0] and buf[1] != 0:
                 return [buf] + compress_helper(s[1:], [s[0]] + [1])
         return compress_helper(s[1:], [s[0]] + [buf[1] + 1])
 
-    def compress_by_section(lst, maxLen):
+    def compress_by_section(lst):
+        '''accept result by compress_helper(), and return binary string translated from base 10. Split the number if the number of  consecutive 1 or 0 is larger than MAX_RUN_LENGTH'''
         if lst == []:
             return ""
-        if lst[0][1] >= maxLen:
-            return "1111100000"  + compress_by_section([lst[0][0] + [lst[0][1]-31]] + lst[1:], maxLen)
-        return completeFiveDigit(numToBinary(lst[0][1] - 1)) + compress_by_section(lst[1:], maxLen)
+        if lst[0][1] > MAX_RUN_LENGTH:
+            return "1111100000" + compress_by_section([[lst[0][0]] + [lst[0][1]-31]] + lst[1:])
+        return completeFiveDigit(numToBinary(lst[0][1])) + compress_by_section(lst[1:])
 
-    return compress_by_section(compress_helper(s, ["0",0]), MAX_RUN_LENGTH)
+    return ("00000" if s[0] == "1" else "") + compress_by_section(compress_helper(s, ["0",0]))
 
 
 
-def decompress(s):
-    def decompress_by_section(s, blockSize):
+def uncompress(s):
+    '''return uncompressed binary'''
+    def uncompress_by_section(s):
+        '''uncompress binary and return a list of numbers of consecutive 0 and 1'''
         if s == "":
             return []
-        print([binaryToNum(s[0:blockSize]) + 1])
-        return [binaryToNum(s[0:blockSize]) + 1] + decompress(s[blockSize:]) # first arg +1
+        return [binaryToNum(s[0:COMPRESSED_BLOCK_SIZE])] + uncompress_by_section(s[COMPRESSED_BLOCK_SIZE:]) # first arg +1
+
+    def uncompress_helper(lst, zero):
+        '''accept result from uncompress_by_section and produce the original binary string according to the array.'''
+        if lst == []:
+            return ""
+        return ("0" if zero else "1" ) * lst[0] + uncompress_helper(lst[1:], not zero)
+
+    return uncompress_helper(uncompress_by_section(s), True)
 
 
-    # def decompress_helper(lst):
-        # if lst == []:
-        #     return ""
-        # return ("0" if zero else "1" )* lst[0]
-
-    return decompress_by_section(s, COMPRESSED_BLOCK_SIZE)
-
-print(compress("0001100000111100001111000011110"))
-print(decompress("0000011111"))
+def compression(s):
+    '''return the compression ratio of my compression algorithm'''
+    return len(compress(s)) / len(s)
 
 
 
